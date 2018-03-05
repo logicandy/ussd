f73d3f278791c791d7cd14ef72319f3455aa2c59
Derrick <euphoriawolf@outlook.com>
2018-03-05 16:09:23 +0100

    moved gasify function to main app.js

--- app2.js
+++ app2.js
@@ -23,45 +23,7 @@ app.get('*', (req, res) => {
 })
 
 
-app.post('/gasify', (req, res) => {
-    let {sessionId, serviceCode, phoneNumber, text} = req.body
 
-    //INITIALIZE RESPONSES
-
-    // This is the first request. Note how we start the response with CON
-    let SizeResponse = `CON Welcome to Gasify, Select your cylinder size:
-    1. 12kg(price)
-    2. 10kg(price)
-    3. 6kg(price)
-    4. 5kg(price)
-    5. 3kg(price)
-    6. other`
-
-    let locationResponse = `CON Kindly select your location
-    1. Gwarimpa
-    2. Jabi
-    3. Wuse
-    4. Lifecamp
-    5. other`
- 
-    //BUSINESS LOGIC+
-
-    if (text == '') {
-      res.send(sizeResponse)
-    } else if (text == '1' || text == '2' || text == '3' ||text=='4'||text=='5') {
-        res.send(locationResponse)
-    } else if (text == '6') {
-      // Business logic for first level response
-      let response = `END We currently don't support any other sizes
-      ,we'll let you know once we do. thanks!`
-      res.send(response)
-    } else if (text == '1*1') {
-      let response = `END your order has been received, you will be contacted soon for confirmation`
-      res.send(response)
-    }  else {
-      res.status(400).send('That option is not available')
-    }
-  })
 
 
 
